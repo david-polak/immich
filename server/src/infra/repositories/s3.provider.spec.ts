35ecf6e5b1d254a1921286ba0bf09dbb97b0c66b
David Polák <git@polakdavid.com>
2023-09-07 23:24:53 +0200

    s3 [ci skip] fc46407a-4dc4-11ee-bddc-f02f7420c3f1

--- server/src/infra/repositories/s3.provider.spec.ts
+++ server/src/infra/repositories/s3.provider.spec.ts
@@ -1,10 +1,14 @@
-import { IStorageRepository } from "@app/domain";
+import { IStorageRepository } from '@app/domain';
+import { S3Provider } from '@app/infra/repositories/s3.provider';
 import { Client, UploadedObjectInfo } from 'minio';
-import { S3Provider } from "@app/infra/repositories/s3.provider";
 
 class S3ProviderMock extends S3Provider implements IStorageRepository {
+  setRemote(client: Client, bucket: string) {
+    this.client = client;
+    this.bucket = bucket;
+  }
   mockClient(fn: any) {
-    fn(this.client)
+    fn(this.client);
   }
   getClient(): Client {
     return this.client;
@@ -16,36 +20,79 @@ describe(S3Provider.name, () => {
   beforeEach(() => {
     provider = new S3ProviderMock();
   });
-  afterEach(() => {})
+  afterEach(() => {});
 
   describe(provider.mkdir.name, () => {
-
     it('creates a directory', () => {
       provider.mockClient((client: Client) => {
-        jest.spyOn(client, 'putObject').mockReturnValue(
-          Promise.resolve({ etag: "dsdgsdg", versionId: null } as UploadedObjectInfo))
-      })
-      provider.mkdir("create/1/directory/")
-    })
+        jest
+          .spyOn(client, 'putObject')
+          .mockReturnValue(Promise.resolve({ etag: 'dsdgsdg', versionId: null } as UploadedObjectInfo));
+      });
+      provider.mkdir('create/1/directory/');
+    });
 
-    it('creates a directory 2', () => {
-      provider.mkdir("create/2/directory/")
-    })
-
-  })
+    it('creates a directory 2', async () => {
+      await provider.mkdir('test/empty/');
+      await provider.mkdir('test/notempty/');
+      // await provider.mkdir("create/2/directory/")
+    });
+  });
 
   describe(provider.readdir.name, () => {
     it('reads dir', async () => {
-      let data = await provider.readdir("create/")
-      console.log(data)
-    })
-  })
+      let data = await provider.readdir('create/');
+      console.log(data);
+    });
+  });
 
   describe(provider.unlink.name, () => {
     it('removes a single file', async () => {
-      await provider.unlink("create/setup")
-    })
-  })
+      await provider.unlink('create/setup');
+    });
+  });
 
+  describe(provider.unlinkDir.name, () => {
+    it('removes an empty directory', async () => {
+      await provider.unlinkDir('test/notempty/');
+      console.log('here');
+    });
+  });
+});
 
-})
+describe(`${S3Provider.name} functional tests`, () => {
+  const S3_BUCKET = process.env.S3_BUCKET || '';
+  const S3_HOSTNAME = process.env.S3_HOSTNAME || '';
+  const S3_PORT = parseInt(process.env.S3_PORT || '443');
+  const S3_USE_SSL = process.env.S3_USE_SSL === 'true';
+  const S3_ACCESS_KEY = process.env.S3_ACCESS_KEY || '';
+  const S3_SECRET_KEY = process.env.S3_SECRET_KEY || '';
+  const client: Client = new Client({
+    endPoint: S3_HOSTNAME,
+    port: S3_PORT,
+    useSSL: S3_USE_SSL,
+    accessKey: S3_ACCESS_KEY,
+    secretKey: S3_SECRET_KEY,
+  });
+
+  const provider: S3ProviderMock = new S3ProviderMock();
+  provider.setRemote(client, S3_BUCKET);
+
+  describe(provider.mkdir.name, () => {
+    it('creates a directory', () => {});
+
+    it('creates a directory 2', async () => {});
+  });
+
+  describe(provider.readdir.name, () => {
+    it('reads dir', async () => {});
+  });
+
+  describe(provider.unlink.name, () => {
+    it('removes a single file', async () => {});
+  });
+
+  describe(provider.unlinkDir.name, () => {
+    it('removes an empty directory', async () => {});
+  });
+});
--- server/src/infra/repositories/s3.provider.ts
+++ server/src/infra/repositories/s3.provider.ts
@@ -1,13 +1,13 @@
 import { DiskUsage, ImmichReadStream, ImmichZipStream, IStorageRepository } from '@app/domain';
-import {BucketItem, BucketStream, Client, UploadedObjectInfo} from 'minio';
-import { Readable } from "stream";
+import { BucketItem, BucketStream, Client, UploadedObjectInfo } from 'minio';
+import { Readable } from 'stream';
 
-const S3_BUCKET = process.env.S3_BUCKET || "";
-const S3_HOSTNAME = process.env.S3_HOSTNAME || "";
-const S3_PORT = parseInt(process.env.S3_PORT || "443");
+const S3_BUCKET = process.env.S3_BUCKET || '';
+const S3_HOSTNAME = process.env.S3_HOSTNAME || '';
+const S3_PORT = parseInt(process.env.S3_PORT || '443');
 const S3_USE_SSL = process.env.S3_USE_SSL === 'true';
-const S3_ACCESS_KEY = process.env.S3_ACCESS_KEY || "";
-const S3_SECRET_KEY = process.env.S3_SECRET_KEY || "";
+const S3_ACCESS_KEY = process.env.S3_ACCESS_KEY || '';
+const S3_SECRET_KEY = process.env.S3_SECRET_KEY || '';
 
 export class S3Provider implements IStorageRepository {
   protected client: Client;
@@ -19,26 +19,26 @@ export class S3Provider implements IStorageRepository {
       port: S3_PORT,
       useSSL: S3_USE_SSL,
       accessKey: S3_ACCESS_KEY,
-      secretKey: S3_SECRET_KEY
-    })
+      secretKey: S3_SECRET_KEY,
+    });
     this.bucket = S3_BUCKET;
   }
 
-  checkDiskUsage(folder: string): Promise<DiskUsage> {
+  async checkDiskUsage(folder: string): Promise<DiskUsage> {
     return Promise.resolve({
-        available: 1000,
-        free: 1000,
-        total: 1000,
+      available: 1000,
+      free: 1000,
+      total: 1000,
     });
   }
 
-  checkFileExists(filepath: string, mode?: number): Promise<boolean> {
+  async checkFileExists(filepath: string, mode?: number): Promise<boolean> {
     return Promise.resolve(false);
   }
 
-  createReadStream(filepath: string, mimeType?: string | null): Promise<ImmichReadStream> {
+  async createReadStream(filepath: string, mimeType?: string | null): Promise<ImmichReadStream> {
     return Promise.resolve({
-      stream: new Readable()
+      stream: new Readable(),
     });
   }
 
@@ -46,47 +46,48 @@ export class S3Provider implements IStorageRepository {
     return {
       stream: new Readable(),
       addFile: () => {},
-      finalize: () => { return Promise.resolve()}
+      finalize: () => {
+        return Promise.resolve();
+      },
     };
   }
 
   async mkdir(filepath: string): Promise<void> {
-    await this.client.putObject(this.bucket, filepath, "", 0)
+    await this.client.putObject(this.bucket, filepath, '', 0);
   }
 
-  moveFile(source: string, target: string): Promise<void> {
+  async moveFile(source: string, target: string): Promise<void> {
     return Promise.resolve(undefined);
   }
 
   async readdir(folder: string): Promise<string[]> {
-    const prefix = folder.endsWith("/") ? folder : `${folder}/`;
+    const prefix = folder.endsWith('/') ? folder : `${folder}/`;
     return new Promise<string[]>((resolve, reject) => {
       const data: string[] = [];
       const stream: BucketStream<BucketItem> = this.client.listObjectsV2(this.bucket, prefix, false);
       stream.on('error', reject);
       stream.on('end', () => resolve(data));
-      stream.on('data', item => {
+      stream.on('data', (item) => {
         if (item.name) {
           // files have names
-          data.push(item.name)
+          data.push(item.name);
         } else if (item.prefix) {
           // directories don't have names, only prefixes
-          data.push(item.prefix)
+          data.push(item.prefix);
         }
-      })
-    })
+      });
+    });
   }
 
-  removeEmptyDirs(folder: string): Promise<void> {
+  async removeEmptyDirs(folder: string): Promise<void> {
     return Promise.resolve(undefined);
   }
 
-  unlink(filepath: string): Promise<void> {
-    return this.client.removeObjects(this.bucket, [filepath])
+  async unlink(filepath: string): Promise<void> {
+    return this.client.removeObjects(this.bucket, [filepath]);
   }
 
-  unlinkDir(folder: string, options?: { recursive?: boolean; force?: boolean }): Promise<void> {
-    return Promise.resolve(undefined);
+  async unlinkDir(folder: string, options?: { recursive?: boolean; force?: boolean }): Promise<void> {
+    return this.client.removeObjects(this.bucket, [folder]);
   }
-
 }
